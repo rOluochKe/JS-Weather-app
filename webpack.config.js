718bb67e4534bc5bf4cf8c73f51d38375b4083fe
Raymond Oluoch <rasholuoch@gmail.com>
2020-06-16 16:14:46 +0300

    updating webpack configuring

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,60 @@
-test;
+const path = require('path');
+
+module.exports = {
+  entry: {
+    index: './src/js/index.js',
+  },
+  output: {
+    filename: 'js/[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' }, { loader: 'css-loader' },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.png/,
+        use: 'file-loader?name=[name].[ext]&outputPath=./images/',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.jpg/,
+        use: 'file-loader?name=[name].[ext]&outputPath=./images/',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: 'file-loader?name=[name].[ext]&outputPath=./images/',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'common',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+};
